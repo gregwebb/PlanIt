4931955788e7c23d04bc38f1a297b9d8714f1600
Greg Webb <gwebb@outlook.com>
2021-07-12 17:27:46 -0600

    adding views for activities

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), 
     # path('about/', views.about, name='about'),
-    # path('activities/', views.activities_index, name='index'), 
-    # path('activities/<int:activity_id>/', views.activities_detail, name='detail'), 
+    path('activities/', views.activities_index, name='index'), 
+    path('activities/<int:activity_id>/', views.activities_detail, name='detail'), 
     # path('activities/create/', views.ActivityCreate.as_view(), name='activities_create'),
     # path('activities/<int:pk>/update/', views.ActivityUpdate.as_view(), name='activities_update'),
     # path('activities/<int:pk>/delete/', views.ActivityDelete.as_view(), name='activities_delete'),
--- main_app/views.py
+++ main_app/views.py
@@ -27,3 +27,13 @@ def signup(request):
 
 def home(request):
   return render(request, 'home.html')
+
+def activities_index(request):
+  activities = Activity.objects.all()
+  return render(request, 'activities/index.html', { 'activities': activities })
+
+def activities_detail(request, activity_id):
+  activity = Activity.objects.get(id=activity_id)
+  return render(request, 'activities/detail.html', {
+    'activity': activity
+  })
