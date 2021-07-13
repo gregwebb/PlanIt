4c86c0ccdf9c1df9df2dc79a5bbb1bd007e11b98
BOSUN KIM <bosunkim@BOSUNs-iMac.local>
2021-07-13 15:41:25 -0700

    forms.py add

---
+++ main_app/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Activity
+
+class ActivityForm(ModelForm):
+    
+    class Meta:
+        model = Activity
+        fields = ['name', 'category', 'date', 'duration', 'start', 'location', 'attendees']
+        widgets = {
+            'date': forms.DateInput(format=('%d-%m-%Y'), attrs={'firstDay': 1, 'pattern=': '\d{4}-\d{2}-\d{2}', 'lang': 'pl', 'format': 'yyyy-mm-dd', 'type': 'date'}),
+        }
