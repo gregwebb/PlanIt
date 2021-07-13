3c6ce48d67077b91dfafd1038ee3611191f3d4dd
Greg Webb <gwebb@outlook.com>
2021-07-13 11:46:39 -0600

    adding proposal model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField, DateTimeRangeField
 from django.contrib.auth.models import User
 
 class Activity(models.Model):
@@ -17,3 +17,10 @@ class Activity(models.Model):
 
   class Meta:
         verbose_name_plural = "activities"
+
+class Proposal(models.Model):
+  activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  suggestion = models.CharField(max_length=255)
+  availability = ArrayField((models.DateTimeRangeField))
+  location = location = models.CharField(max_length=255)
