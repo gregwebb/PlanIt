e143d7c27336e774814baff9f6ddf9b5147d5646
Mike Mattingly <cmattingly1584@gmail.com>
2021-07-13 16:36:48 -0700

    adding Google Key

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from .models import Activity, Proposal
 
 admin.site.register(Activity)
-<<<<<<< HEAD
 admin.site.register(Proposal)
-=======
-admin.site.register(Proposal)
->>>>>>> 43f7118911bc4ee754eea11a465418c220621675
