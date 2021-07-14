8c64e9e87f418bd9d1f452f11bf3e3c9b41e66a5
Greg Webb <gwebb@outlook.com>
2021-07-14 00:29:28 -0600

    adding map to activity view

--- planit/settings.py
+++ planit/settings.py
@@ -11,17 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import json
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-with open(BASE_DIR/'config.json') as config_file: 
-    config = json.load(config_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=uv)@lkg#k43wkd=z$@ww7tnmrxk$l#*xhf#nv$mir2zk-aigw'
